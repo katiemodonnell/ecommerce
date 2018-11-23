f32c4c72b64c753c8ca5667d6155215f165960fc
Katie O Donnell <katiemodonnell88@gmail.com>
2018-11-23 14:22:20 +0000

    whitenoise installed

--- django_ecommerce/settings/dev.py
+++ django_ecommerce/settings/dev.py
@@ -3,3 +3,4 @@ from .base import *
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
